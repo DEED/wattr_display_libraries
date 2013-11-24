1efc458b5e19c53e059855a46b2efcc1bc6ef70b
DEED <deltaalphanovember@gmail.com>
2013-11-24 12:46:08 -0500

    Added some more GUI functions
    Will work on implementing state machine next.

--- GUI_library.h
+++ GUI_library.h
@@ -4,6 +4,18 @@
 #ifndef GUI_LIBRARY_H
 #define GUI_LIBRARY_H
 
+enum measurements{
+	VOLTAGE,
+	CURRENT,
+	KWH,
+	COST,
+	WATTS,
+	VOLTAMPS,
+	VARS,
+	FREQ,
+	DEGREES,
+	PFACTOR
+};
 
 void draw_selection_screen(void);
 void initial_screen(void);
@@ -11,5 +23,11 @@ void draw_device_info(void);
 void draw_measurements_screen(int cursor_row);
 void measurement_todraw(int measurement_number);
 void draw_settings_screen(void);
+void draw_set_clock_screen(void);
+void draw_set_cost_screen(void);
+void draw_set_energy_cycles_screen(void);
+void draw_set_spike_level_screen(void);
+void draw_set_sag_cycles_screen(void);
+void set_sag_level_screen(void);
 
 #endif
